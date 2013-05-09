c419de1314dab9b10c976858a2c63364a54db095
Sachin Ravi <sachinravi14@gmail.com>
2013-05-09 14:13:29 -0400

    Reading from latencies file and creating distribution according to data provided; also adding the .txt file itself

--- dist.py
+++ dist.py
@@ -3,6 +3,60 @@ from scipy.stats import invgauss
 from random import randrange 
 import math as math
 
+import csv
+
+def getDistFromFile():
+	distList = []
+	muSigmaList = []
+	
+	numArms = 760
+	numRounds = 1361
+	
+	# empty list for each round
+	for roundIndex in range(0, numRounds):
+		distList.append([])
+	
+	# csv reader to read from text file
+	filename = "univ-latencies.txt"
+	file = open(filename, "r")
+	csv_reader = csv.reader(file)
+	
+	# Read rewards from text file
+	lineCount = 0
+	for line in csv_reader:
+		lineCount += 1
+		
+		# First line is just list of universities, so skip
+		if lineCount == 1:
+			continue
+		else:
+			# Each line contains the rewards for a certain round
+			roundIndex = 0
+			for reward in line:
+				distList[lineCount - 2].append(float(reward))
+		
+	# Calculate mean and standard deviation from reward info
+	for armIndex in range(0, numArms):
+		# Mean:
+		mean = 0.0
+		for roundIndex in range(0, numRounds):
+			mean += distList[roundIndex][armIndex]
+		
+		mean = mean/numRounds;
+		
+		# Std Deviation:
+		stdDev = 0.0
+		for roundIndex in range(0, numRounds):
+			stdDev += (distList[roundIndex][armIndex] - mean)*(distList[roundIndex][armIndex] - mean)
+			
+		stdDev = math.sqrt(stdDev/numRounds)
+		
+		# Record mean and std dev for each arm
+		muSigmaList.append((mean, stdDev))
+		
+	return distList, muSigmaList
+	
+
 # Generates the distribution on the arms with some probability
 # Returns list of lists where first level list is by round
 # and within each list, it is the number produced by an arm 
@@ -93,3 +147,5 @@ if __name__ == '__main__':
 	dlist, meanList = getDist(5, 10)
 	for list in dlist:
 		print list
+		
+	getDistFromFile()
